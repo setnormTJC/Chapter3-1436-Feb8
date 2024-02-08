c6ed3864b6fba1f9c8cac2dbf03032262d1fdcbd
Work <Work@MSI>
2024-02-08 10:15:05 -0600

    Add project files.

---
+++ Chapter3-1436-Feb8/StartingChapter3-Feb8.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+
+#define print(text) std::cout << text << std::endl
+
+int main()
+{
+    cout << "adfasd" << endl;
+
+    print("asdfasdfasdfasdfasdfasdfsdasdfd");
+
+    //std::
+    system("pause"); 
+
+
+    cout << pow(2, 3) << endl; //raise FIRST input to the power of the second input
+
+    return 0;
+}
